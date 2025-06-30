73065a58d4f481f41823956596737709f60f2562
Sachin Motwani <sachinmotwani60@gmail.com>
2025-07-01 00:44:39 +0530

    viteconfig added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app-icon-mockup/',
+  base: '/', // 👈 critical when using proxy
   build: {
     outDir: 'dist',
-  }
-})
+  },
+});
